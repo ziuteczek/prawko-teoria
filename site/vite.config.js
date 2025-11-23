8fa2a307952cb2410b85f2f21b3ecbae987b9545
Stanislaw Dymny <dymnystan@gmail.com>
2025-11-23 17:46:16 +0100

    site init

---
+++ site/vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from 'path'
+
+export default {
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: '../dist'
+  },
+  server: {
+    port: 8080
+  },
+  // Optional: Silence Sass deprecation warnings. See note below.
+  css: {
+     preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            'import',
+            'mixed-decls',
+            'color-functions',
+            'global-builtin',
+          ],
+        },
+     },
+  },
+}
