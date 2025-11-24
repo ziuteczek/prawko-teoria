baa936e719c9b76c345ed5008b15efdd2ff4c5bd
Stanislaw Dymny <dymnystan@gmail.com>
2025-11-24 11:17:19 +0100

    contact form added

--- site/src/js/main.js
+++ site/src/js/main.js
@@ -1,3 +1,28 @@
-import '../scss/styles.scss'
+import emailJs from "@emailjs/browser";
+// import "../scss/styles.scss";
 
-import * as bootstrap from 'bootstrap'
+// import * as bootstrap from "bootstrap";
+
+const currYearEl = document.querySelectorAll(".current-year");
+const currYear = new Date().getFullYear();
+
+currYearEl.forEach((el) => (el.textContent = currYear));
+
+emailJs.init({ publicKey: "07SD-PGGed1yctGgp" });
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async function (e) {
+	e.preventDefault();
+
+	try {
+		const res = await emailJs.sendForm(
+			"service_6njdcge",
+			"template_hk312am",
+			this
+		);
+		console.log(res);
+	} catch (err) {
+		console.error(err);
+	}
+});
