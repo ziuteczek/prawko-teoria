5c6a042096055648ea775506ecb90f2c46317291
Stanislaw Dymny <dymnystan@gmail.com>
2025-12-11 15:34:01 +0100

    HOTFIX: nav removed from login & register page

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -6,33 +6,43 @@ import IndexRoute from "../routes/index.route";
 import Quiz from "../features/quiz/components/Quiz";
 import { AuthProvider } from "../providers/auth.provider";
 import { PreloadProvider } from "../providers/preload.provider";
-import "../styles/global.css"
+import "../styles/global.css";
 import MainLaout from "../features/layout/components/MainLayout";
 import Logout from "./pages/logout";
 import QuestionsList from "../features/questions-list/components/page";
 
 function App() {
-    return (
-        <>
-            <AuthProvider>
-                <PreloadProvider>
-                    <BrowserRouter>
-                        <Routes>
-                            <Route element={<MainLaout/>}>
-                                <Route path="/" index element={<IndexRoute />} />
-                                <Route path="/login" element={<Login />} />
-                                <Route path="/dashboard" element={<Dashboard />} />
-                                <Route path="/register" element={<Register />} />
-                                <Route path="/quiz" element={<Quiz />} />
-                                <Route path="/logout" element={<Logout/>} />
-                                <Route path="/list" element={<QuestionsList/>} />
-                            </Route>
-                        </Routes>
-                    </BrowserRouter>
-                </PreloadProvider>
-            </AuthProvider>
-        </>
-    );
+	return (
+		<>
+			<AuthProvider>
+				<PreloadProvider>
+					<BrowserRouter>
+						<Routes>
+							<Route path="/register" element={<Register />} />
+							<Route path="/login" element={<Login />} />
+							<Route path="/logout" element={<Logout />} />
+							<Route element={<MainLaout />}>
+								<Route
+									path="/"
+									index
+									element={<IndexRoute />}
+								/>
+								<Route
+									path="/dashboard"
+									element={<Dashboard />}
+								/>
+								<Route path="/quiz" element={<Quiz />} />
+								<Route
+									path="/list"
+									element={<QuestionsList />}
+								/>
+							</Route>
+						</Routes>
+					</BrowserRouter>
+				</PreloadProvider>
+			</AuthProvider>
+		</>
+	);
 }
 
 export default App;
